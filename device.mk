d7b565e51fb49e7aa7a37654f2f09b69e38358d9
sjllls <2012ji123@gmail.com>
2021-05-09 08:49:50 +0800

    akatsuki: Initial commit

---
+++ device.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2018-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Get non-open-source specific aspects
+$(call inherit-product-if-exists, vendor/sony/akatsuki/akatsuki-vendor.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2880
+TARGET_SCREEN_WIDTH := 1440
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Inherit from tama-common
+$(call inherit-product, device/sony/tama-common/common.mk)
